ea050dcea658d02075bb99de8bce0cd9a5df7a33
ouram <mboivin@student.42.fr>
2020-07-20 16:16:53 +0200

    Add event types and masks

--- includes/minirt_define.h
+++ includes/minirt_define.h
@@ -6,15 +6,61 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/20 15:49:09 by mboivin           #+#    #+#             */
-/*   Updated: 2020/07/20 15:50:31 by mboivin          ###   ########.fr       */
+/*   Updated: 2020/07/20 16:16:27 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINIRT_DEFINE_H
 # define MINIRT_DEFINE_H
 
+/*
+** Default window render size and title
+*/
+
 # define RENDER_X 840
 # define RENDER_Y 680
 # define WIN_TITLE "miniRT"
+
+/*
+** Event masks
+*/
+
+# define KEYPRESSMASK (1L<<0)
+# define KEYRELEASEMASK (1L<<1)
+# define DESTROYNOTIFYMASK (1L << 17)
+
+/*
+** Event types
+*/
+
+# define KEYPRESS 2
+# define KEYRELEASE 3
+# define BUTTONPRESS 4
+# define BUTTONRELEASE 5
+# define MOTIONNOTIFY 6
+# define ENTERNOTIFY 7
+# define LEAVENOTIFY 8
+# define FOCUSIN 9
+# define FOCUSOUT 10
+# define KEYMAPNOTIFY 11
+# define EXPOSE 12
+# define GRAPHICSSEXPOSE 13
+# define NOEXPOSE 14
+# define VISIBILITYNOTIFY 15
+# define CREATENOTIFY 16
+# define DESTROYNOTIFY 17
+# define UNMAPNOTIFY 18
+# define MAPNOTIFY 19
+# define MAPREQUEST 20
+# define REPARENTNOTIFY 21
+# define CONFIGURENOTIFY 22
+# define CONFIGUREREQUEST 23
+# define GRAVITYNOTIFY 24
+
+/*
+** TTY function keys
+*/
+
+# define ESC_KEY 65307
 
 #endif
