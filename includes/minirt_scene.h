a36022c94417e45674c84b5b233b74a195aa7acc
matboivin <mboivin@student.42.fr>
2020-07-23 00:56:42 +0200

    Create light

--- includes/minirt_scene.h
+++ includes/minirt_scene.h
@@ -6,7 +6,7 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/20 16:25:15 by mboivin           #+#    #+#             */
-/*   Updated: 2020/07/23 00:11:36 by mboivin          ###   ########.fr       */
+/*   Updated: 2020/07/23 00:55:26 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,16 +34,25 @@ typedef struct	s_cam
 	double		fov;
 }				t_cam;
 
+typedef struct	s_light
+{
+	t_coord3	pos;
+	t_coord3	rot;
+	t_color		color;
+}				t_light;
+
 typedef struct	s_scene
 {
 	t_res		res;
 	t_amb		amb;
 	t_cam		cam;
+	t_light		light;
 }				t_scene;
 
 void			create_resolution(t_res *res);
 void			create_ambient(t_amb *amb);
 void			create_cam(t_cam *cam);
+void			create_light(t_light *light);
 void			create_scene(t_scene *scene);
 void			destroy_scene(t_scene *to_destroy);
 
--- src/scene.c
+++ src/scene.c
@@ -6,7 +6,7 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/20 16:29:07 by mboivin           #+#    #+#             */
-/*   Updated: 2020/07/23 00:21:10 by mboivin          ###   ########.fr       */
+/*   Updated: 2020/07/23 00:56:27 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,11 +26,18 @@ void	create_ambient(t_amb *amb)
 	amb->color = create_color(0, 0, 0);
 }
 
-void			create_cam(t_cam *cam)
+void	create_cam(t_cam *cam)
 {
 	cam->pos = create_coord3(0, 0 ,0);
 	cam->rot = create_coord3(0, 0 ,0);
 	cam->fov = 0.0;
+}
+
+void	create_light(t_light *light)
+{
+	light->pos = create_coord3(0, 0 ,0);
+	light->rot = create_coord3(0, 0 ,0);
+	light->color = create_color(0, 0, 0);
 }
 
 void	create_scene(t_scene *scene)
@@ -38,6 +45,7 @@ void	create_scene(t_scene *scene)
 	create_resolution(&scene->res);
 	create_ambient(&scene->amb);
 	create_cam(&scene->cam);
+	create_light(&scene->light);
 }
 
 void	destroy_scene(t_scene *to_destroy)
