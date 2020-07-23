7c3430b86a27769d83e2731965ae28c5693f9277
matboivin <mboivin@student.42.fr>
2020-07-23 23:32:51 +0200

    Fix leak

--- src/parsing.c
+++ src/parsing.c
@@ -6,39 +6,39 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/22 15:01:06 by mboivin           #+#    #+#             */
-/*   Updated: 2020/07/23 01:23:35 by mboivin          ###   ########.fr       */
+/*   Updated: 2020/07/23 23:31:35 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
 /*
-** Array of structures: pointers to parsing functions matching identifier
+** Array of structures: Parsing functions matching identifier
 */
 
-struct s_parse_tab	g_parse_tab[] =
+struct s_pars_tab	g_pars_tab[] =
 {
-	{ "R " , &get_resolution },
-	{ "A " , &get_ambient },
-	{ "c " , &get_cam },
-	{ "l " , &get_light },
-	{ "sp" , &get_sphere },
-	{ "pl" , &get_plane },
-	{ "sq" , &get_square },
-	{ "cy" , &get_cylinder },
-	{ "tr" , &get_triangle }
+	{ "R ", &get_resolution },
+	{ "A ", &get_ambient },
+	{ "c ", &get_cam },
+	{ "l ", &get_light },
+	{ "sp", &get_sphere },
+	{ "pl", &get_plane },
+	{ "sq", &get_square },
+	{ "cy", &get_cylinder },
+	{ "tr", &get_triangle }
 };
 
-static void	call_parsing_func(t_scene *scene, char **input)
+static void		call_parsing_func(t_scene *scene, char **input)
 {
-	int		i;
+	int			i;
 
 	i = 0;
 	while (i < 9)
 	{
-		if (ft_strnequ(g_parse_tab[i].u_id, *input, 2))
+		if (ft_strnequ(g_pars_tab[i].u_id, *input, 2))
 		{
-			(*g_parse_tab[i].func)(scene, input);
+			(*g_pars_tab[i].func)(scene, input);
 			return ;
 		}
 		i++;
@@ -46,25 +46,44 @@ static void	call_parsing_func(t_scene *scene, char **input)
 	exit_error(scene, ID_ERRR);
 }
 
-/* 
-Double:	0.2
-Double:	0
-
-Coord3:	5.0,3.0,2.0
-Coord3:	-5.0,3.0,2.0
-Coord3:	0,0,1
-
-Color:	255,255,255
-
-Int:	1920
+/*
+** Function: Gets the entire scene description
 */
 
-void		parse_scene(t_scene *scene, const char *filepath)
+char			*read_scene_file(t_scene *scene, const char *filepath)
 {
-	char	*input;
+	int			fd;
+	int			bytes_read;
+	char		buffer[BUFFER_SIZE + 1];
+	char		*result;
+
+	result = NULL;
+	fd = open(filepath, O_RDONLY);
+	if (fd < 0)
+		exit_error(scene, DEFAULT_ERR);
+	while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) > 0)
+	{
+		buffer[bytes_read] = '\0';
+		result = ft_strjoindelone(result, buffer);
+		if (result == NULL)
+			exit_error(scene, DEFAULT_ERR);
+	}
+	close(fd);
+	return (result);
+}
+
+/*
+** Function: Parse the scene description to fill the scene struct
+*/
+
+void			parse_scene(t_scene *scene, const char *filepath)
+{
+	char		*input;
+	char		*head;
 	const char	*ids = "RAclspt";
 
 	input = read_scene_file(scene, filepath);
+	head = input;
 	while (*input)
 	{
 		skip_whitespaces(&input);
@@ -74,37 +93,6 @@ void		parse_scene(t_scene *scene, const char *filepath)
 			exit_error(scene, SCENE_FMT);
 	}
 	resize_window();
+	free(head);
 	// TODO: Check R, A and c not missing, and scene is not in the complete dark
-}
-
-/*
-** Function: Gets the entire scene
-*/
-
-// TODO: Debug leak
-
-char		*read_scene_file(t_scene *scene, const char *filepath)
-{
-	int		fd;
-	int		ret_value;
-	char	*line;
-	char	*result;
-
-	ret_value = 1;
-	line = NULL;
-	result = NULL;
-	fd = open(filepath, O_RDONLY);
-	if (fd < 0)
-		exit_error(scene, DEFAULT);
-	while (ret_value)
-	{
-		ret_value = get_next_line(fd, &line);
-		if (!(result = ft_strjoindelone(result, line)))
-			exit_error(scene, DEFAULT);
-		free(line);
-	}
-	if (ret_value == -1)
-		exit_error(scene, DEFAULT);
-	close(fd);
-	return (result);
 }
