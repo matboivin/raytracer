13f91fa54e678b7ce1f1af796c5c85e34b0d2417
matboivin <mboivin@student.42.fr>
2020-08-11 21:29:45 +0200

    Remove extra functions

--- includes/minirt_transform.h
+++ includes/minirt_transform.h
@@ -6,7 +6,7 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/20 16:25:15 by mboivin           #+#    #+#             */
-/*   Updated: 2020/08/11 16:18:21 by mboivin          ###   ########.fr       */
+/*   Updated: 2020/08/11 21:29:29 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,5 @@
 t_mat4x4	create_worldtocam(t_vec3 from, t_vec3 cam_dir, t_vec3 world_up);
 t_mat4x4	create_camtoworld(t_vec3 from, t_vec3 cam_dir, t_vec3 world_up);
 t_mat4x4	look_at(t_cam *cam);
-
-void		cam_rotation_x(t_mat4x4 *mat, double x);
-void		cam_rotation_y(t_mat4x4 *mat, double y);
-void		cam_rotation_z(t_mat4x4 *mat, double z);
-t_mat4x4	rotate_cam(t_vec3 dir);
-
-double		degrees_to_radians(double deg_angle);
-double		radians_to_degrees(double rad_angle);
 
 #endif
