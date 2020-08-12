e39f93a138a4e25d10b8de5d077ebed8ee70809b
matboivin <mboivin@student.42.fr>
2020-08-12 09:32:59 +0200

    Move normalize forward

--- src/transform/cam_to_world.c
+++ src/transform/cam_to_world.c
@@ -6,7 +6,7 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/20 21:38:29 by mboivin           #+#    #+#             */
-/*   Updated: 2020/08/12 09:22:52 by mboivin          ###   ########.fr       */
+/*   Updated: 2020/08/12 09:32:02 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,10 +30,9 @@ t_mat4x4		create_worldtocam(t_vec3 from, t_vec3 cam_dir, t_vec3 world_up)
 	t_vec3		up;
 	t_vec3		right;
 
-	forward = cam_dir;
+	forward = normalize_vec3(cam_dir);
 	right = normalize_vec3(cross(world_up, forward));
 	up = normalize_vec3(cross(forward, right));
-	forward = normalize_vec3(forward);
 	result.c1 = create_vec4(right.x, right.y, right.z, 0.0);
 	result.c2 = create_vec4(up.x, up.y, up.z, 0.0);
 	result.c3 = create_vec4(forward.x, forward.y, forward.z, 0.0);
