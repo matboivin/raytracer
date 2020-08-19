64e7990a1416f99a6010d1e92d8c89bb7a829f7e
matboivin <mboivin@student.42.fr>
2020-08-20 01:29:54 +0200

    Wip shapes

--- src/rendering/intersect_cylinder.c
+++ src/rendering/intersect_cylinder.c
@@ -6,7 +6,7 @@
 /*   By: mboivin <mboivin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/05 01:58:17 by mboivin           #+#    #+#             */
-/*   Updated: 2020/08/20 00:21:35 by mboivin          ###   ########.fr       */
+/*   Updated: 2020/08/20 01:17:19 by mboivin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ static t_vec3	set_vec3_ynull(t_vec3 v)
 /*
 ** This function computes cylinder's normal
 */
-
+/*
 static t_vec3	get_cylinder_normal(t_vec3 v, t_vec3 dir)
 {
 	t_vec3		result;
@@ -40,6 +40,7 @@ static t_vec3	get_cylinder_normal(t_vec3 v, t_vec3 dir)
 	result = normalize_vec3(result);
 	return (result);
 }
+*/
 
 /*
 ** This function handles intersection with a cylinder
@@ -50,8 +51,8 @@ bool			intersect_cylinder(t_cyl *cylinder, t_ray *ray)
 {
 	t_vec3		quad_coef;
 	t_vec3		inter_p;
-	t_vec3		v;
-	t_vec3		normal;
+	t_vec3		zmin;
+	t_vec3		zmax;
 
 	quad_coef = get_quad_coef(
 		set_vec3_ynull(ray->origin),
@@ -61,9 +62,10 @@ bool			intersect_cylinder(t_cyl *cylinder, t_ray *ray)
 	if (solve_quadratic(ray, quad_coef) == true)
 	{
 		inter_p = get_intersection_point(ray);
-		v = sub_vec3(inter_p, cylinder->base1);
-		normal = get_cylinder_normal(v, cylinder->dir);
-		if (dot_vec3(normal, v) > 0)
+		zmin = sub_vec3(inter_p, cylinder->base1);
+		zmax = sub_vec3(inter_p, cylinder->base2);
+		if ((dot_vec3(cylinder->dir, zmin) > 0)
+			&& (dot_vec3(cylinder->dir, zmax) < 0))
 			return (true);
 		reset_ray_dist(ray);
 	}
